9d96928c6fd869bbf6797fad15a8bb6bfc888e59
Eric Halverson <ericleifhalverson@gmail.com>
2023-01-22 17:05:18 -0800

    Add get and post routes for user sign_up and sign_in pointing to new registrations and sessions controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  # GET /about
-  get "about-us", to: "about#index", as: "about"
+  get "about", to: "about#index", as: :about
 
-  # Defines the root path route ("/")
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+ 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: "sessions#destroy"
+
   root "main#index"
 end
